03453cab68f1769d002095cb396bca75baedc25c
akshay343 <127043464+Akshay-343@users.noreply.github.com>
2023-12-17 17:57:09 +0530

    Add files via upload

--- crafts/admin.py
+++ crafts/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Artisan, Customer, Product
 
-# Register your models here.
+# Register your models with the admin site
+admin.site.register(Product)
+admin.site.register(Artisan)
+admin.site.register(Customer)
--- crafts/views.py
+++ crafts/views.py
@@ -14,6 +14,9 @@ def artisan_signup(request):
 
         if user_form.is_valid() and artisan_form.is_valid():
             user = user_form.save(commit=False)
+
+
+
             user.set_password(user_form.cleaned_data['password'])
             user.save()
 
@@ -59,23 +62,34 @@ def customer_signup(request):
     return render(request, 'customer_signup.html', {'user_form': user_form, 'customer_form': customer_form})
 
 
+from django.shortcuts import render, redirect
+from .forms import ProductForm
+from .models import Product
+
+
 def add_product(request):
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            product = form.save()
-            return redirect('success_page')  # Replace 'success_page' with your actual success page URL
+            # Assuming the logged-in user is an artisan
+            artisan = request.user.artisan
+            product = form.save(commit=False)
+
+            # Assign the artisan to the product before saving
+            product.artisan = artisan
+            product.save()
+
+            return redirect('home')  # Replace 'success_page' with your actual success page URL
 
     else:
         form = ProductForm()
 
-    return render(request, 'create_product.html', {'form': form})
-
+    return render(request, 'add_product.html', {'form': form})
 
 
 def logout_view(request):
     logout(request)
-    return redirect('home')  # Redirect to the appropriate page after logout
+    return redirect('home')
 
 
 def artisan_login(request):
@@ -86,9 +100,12 @@ def artisan_login(request):
             password = form.cleaned_data['password']
             user = authenticate(request, username=username, password=password)
 
-            if user is not None and user.artisan is not None:
-                login(request, user)
-                return redirect('home')  # Redirect to the appropriate page
+            if user is not None:
+                if hasattr(user, 'artisan') and user.artisan is not None:
+                    login(request, user)
+                    return redirect('home')  # Redirect to the appropriate page
+                else:
+                    error = 'User has no artisan profile.'
             else:
                 error = 'Invalid login credentials'
         else:
@@ -98,6 +115,7 @@ def artisan_login(request):
         error = None
 
     return render(request, 'artisan_login.html', {'form': form, 'error': error})
+
 
 def customer_login(request):
     if request.method == 'POST':
